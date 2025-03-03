685f2ac197793fa88a0447f5df61f159276826cf
Dylan Ponsford <60271425+dylanxp@users.noreply.github.com>
2025-03-03 23:41:12 +0000

    restructure code into src directory

---
+++ subteams/LLMProbing/src/probes/__init__.py
@@ -0,0 +1,4 @@
+"""
+This module contains code for probe classes, and the training and evaluation of these probes
+TODO: Some restructuring and renaming required in order to have better nomenclature
+"""
---
+++ subteams/LLMProbing/src/samples/__init__.py
@@ -0,0 +1,4 @@
+"""
+This module contains code for generating samples, both manually and randomly via the odeformer methodology
+TODO: Some restructuring and renaming required in order to have better nomenclature
+"""
