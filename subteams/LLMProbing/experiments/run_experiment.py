3fce39347383a8d2b265ece25ff3a595ae349755
Dylan Ponsford <60271425+dylanxp@users.noreply.github.com>
2025-03-04 00:06:45 +0000

    add WIP experiment runner

---
+++ subteams/LLMProbing/experiments/run_experiment.py
@@ -0,0 +1,65 @@
+import os
+import pandas as pd
+from torch.utils.data import DataLoader
+from src.datasets.activations_dataset import ActivationsDataset
+from src.datasets.utils import split_dataset, get_d_in
+from src.probes.lr_probe import LRProbe
+from src.probes.utils import train_probe, eval_probe, save_probe_to_path
+
+def train_and_save_probe_separation_expt(target_layer_idx, target_feature, activations_path, \
+                                         probe_name, probes_path, \
+                                         lr, num_epochs, \
+                                         shuffle_datasets = True, use_val = True, data_split=[0.8, 0.1, 0.1], write_data=False):
+  """
+  TODO: add description
+  """
+  # Test dataset, dataloaders, and splitting
+  full_dataset = ActivationsDataset(activations_path=activations_path, feature_label=target_feature, layer_idx=target_layer_idx)
+  train_dataset, val_dataset, test_dataset = split_dataset(full_dataset, lengths=data_split)
+  train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=shuffle_datasets)
+  if use_val:
+    val_dataloader = DataLoader(val_dataset, shuffle=shuffle_datasets)
+  test_dataloader = DataLoader(test_dataset, shuffle=shuffle_datasets)
+
+  d_in = get_d_in(full_dataset)
+  probe = LRProbe(d_in)
+
+  # Training loop
+  if use_val:
+    probe, train_losses, train_accuracies, val_losses, val_accuracies = train_probe(probe, train_dataloader, lr=lr, write_log=write_data, num_epochs=num_epochs, val_dataloader=val_dataloader)
+  else:
+    probe, train_losses, train_accuracies, val_losses, val_accuracies = train_probe(probe, train_dataloader, lr=lr, write_log=write_data, num_epochs=num_epochs)
+
+  # Evaluation on test set
+  test_loss, test_acc, test_fail_ids = eval_probe(probe, test_dataloader)
+  print(f'\nProbe trained on layer {target_layer_idx}:')
+  print(f'Test Set: Loss {test_loss}, Accuracy {test_acc}')
+
+  # Save probe
+  probe_path = os.path.join(probes_path, probe_name)
+  save_probe_to_path(probe, probe_path)
+
+  return test_loss, test_acc, test_fail_ids
+
+def separability_testing(target_feature, activations_path, \
+                         lr, num_epochs, \
+                         layers, num_repeats=1):
+  """
+  TODO: add description
+  """
+  experiment_data = []
+
+  for layer_idx in layers:
+    for run in range(num_repeats):
+      probe_name = f'probe_{target_feature}_{layer_idx}_{run}.pt'
+      test_loss, test_acc, test_fail_ids = train_and_save_probe_separation_expt(layer_idx, target_feature, lr, num_epochs, probe_name)
+      experiment_data.append({
+            "layer": layer_idx,
+            "run": run,
+            "accuracy": test_acc,
+            "loss": test_loss
+        })
+
+  experiment_data = pd.DataFrame(data=experiment_data)
+
+  return experiment_data
