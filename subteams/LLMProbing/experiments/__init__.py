a833f1176c83192e2486e28bed621c588d8b9d9c
Dylan Ponsford <60271425+dylanxp@users.noreply.github.com>
2025-03-10 20:16:27 +0000

    import separability_testing experiment in init

--- subteams/LLMProbing/experiments/__init__.py
+++ subteams/LLMProbing/experiments/__init__.py
@@ -1,3 +1,7 @@
 """
 This module contains code for running experiments using the probing pipeline
 """
+
+from .run_experiment import separability_testing  # Import separability_testing from run_experiment.py
+
+__all__ = ["separability_testing"]  # Defines what gets imported with "from experiments import *"
